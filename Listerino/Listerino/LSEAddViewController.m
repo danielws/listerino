7ca65f2bbff14d563110e0b98285d31624f7d7fb
Ben Langholz <blangholz@gmail.com>
2014-03-19 19:11:47 -0700

    Basic Layout

--- Listerino/Listerino/LSEAddViewController.m
+++ Listerino/Listerino/LSEAddViewController.m
@@ -10,6 +10,18 @@
 
 @interface LSEAddViewController ()
 @property (nonatomic, strong) UIButton *doneButton;
+@property (nonatomic,strong) UIImageView *imageView;
+@property (nonatomic,strong) UIButton *cameraButton;
+@property (nonatomic,strong) UIButton *cameraRollButton;
+@property (nonatomic,strong) UIView *locationView;
+@property (nonatomic, strong) UIImageView *locationIconView;
+@property (nonatomic, strong) UIImageView *chevronIconView;
+@property (nonatomic,strong) UITextView *locationText;
+@property (nonatomic, strong) UIView *noteView;
+@property (nonatomic,strong) UITextView *noteText;
+@property (nonatomic,strong) UIScrollView *noteScrollView;
+@property (nonatomic, strong) UIColor *noteColor;
+@property (nonatomic, strong) UIView *locationDivider;
 
 
 @end
@@ -30,6 +42,59 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor redColor];
+
+    //Note Color
+    self.noteColor = [UIColor redColor];
+        
+    self.view.backgroundColor = self.noteColor;
+    
+    // Inits -------------------------------------------------------
+    
+    //Note scroll view
+    self.noteScrollView = [[UIScrollView alloc] initWithFrame:CGRectZero];
+    [self.view addSubview: self.noteScrollView];
+    
+    // Note Image
+    self.imageView = [[UIImageView alloc] initWithImage:nil];
+    [self.noteScrollView addSubview:self.imageView];
+    
+    //Camera Button
+    self.cameraButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview: self.cameraButton];
+
+    //Camera Button
+    self.cameraRollButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview: self.cameraRollButton];
+    
+    // Location Label
+    self.locationView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.noteScrollView addSubview:self.locationView];
+    
+    // Location Icon
+    UIImage *locationIcon = [UIImage imageNamed:@"location.png"];
+    self.locationIconView = [[UIImageView alloc] initWithImage:locationIcon];
+    [self.locationView addSubview:self.locationIconView];
+    
+    // Chevron Icon
+    UIImage *chevronIcon = [UIImage imageNamed:@"chevron.png"];
+    self.chevronIconView = [[UIImageView alloc] initWithImage:chevronIcon];
+    [self.locationView addSubview:self.chevronIconView];
+    
+    // Location Text
+    self.locationText = [[UITextView alloc] initWithFrame:CGRectZero];
+    self.locationText.editable = NO;
+    self.locationText.text = @"location";
+    [self.locationView addSubview:self.locationText];
+    
+    // Note Text
+    self.noteView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.noteView addSubview:self.noteText];
+    [self.noteScrollView addSubview:self.noteView];
+    
+    self.noteText = [[UITextView alloc] initWithFrame:CGRectZero];
+    self.noteText.text = @"Note";
+    [self.noteView addSubview:self.noteText];
+
     
     self.doneButton = [[UIButton alloc]initWithFrame:CGRectMake((self.view.frame.size.width - 80) / 2, CGRectGetMaxY(self.view.frame) - 100, 80, 80)];
     UIImage *addButtonImage = [UIImage imageNamed:@"doneButton.png"];
@@ -58,6 +123,56 @@
 }
 */
 
+#pragma mark - View layout
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    // Image view layout
+    _imageView.frame = CGRectMake(0, 0, 320, 240);
+    _imageView.backgroundColor = _noteColor;
+    _imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    //Camera Button Layout
+    [_cameraButton setTitle:@"Take Photo" forState:UIControlStateNormal];
+    _cameraButton.frame = CGRectMake(0, 0, 320, 100);
+    [_cameraButton addTarget:self action:@selector(onCameraButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //Camera Roll Layout
+    [_cameraRollButton setTitle:@"Choose Photo" forState:UIControlStateNormal];
+    _cameraRollButton.frame = CGRectMake(0, 100, 320, 100);
+    [_cameraRollButton addTarget:self action:@selector(onCameraRollButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Location view layout
+    _locationView.frame = CGRectMake(0, CGRectGetMaxY(_imageView.frame), 320, 50);
+    _locationView.backgroundColor = [UIColor colorWithWhite:1 alpha:.1];
+    
+    _locationIconView.frame = CGRectMake(8-8, 8, _locationView.frame.size.height - 14, _locationView.frame.size.height-16);
+    _locationIconView.contentMode = UIViewContentModeCenter;
+    //    _locationIconView.backgroundColor = [UIColor grayColor];
+    
+    _chevronIconView.frame = CGRectMake(CGRectGetWidth(_locationView.frame) - CGRectGetWidth(_locationIconView.frame)-8+7, 8, CGRectGetHeight(_locationIconView.frame), CGRectGetWidth(_locationIconView.frame));
+    _chevronIconView.contentMode = UIViewContentModeCenter;
+    
+    _locationText.frame = CGRectMake(16 + (_locationView.frame.size.height - 16 - 13), 10, CGRectGetWidth(_locationView.frame) - 24 - (_locationIconView.frame.size.width)*2-8 +46, CGRectGetHeight(_locationView.frame)- 16);
+    _locationText.backgroundColor = [UIColor clearColor];
+    _locationText.font = [UIFont fontWithName:@"HelveticaNeue" size:14];
+    _locationText.textColor = [UIColor whiteColor];
+    
+    _noteText.frame = CGRectMake(10, 10, 320 - 20, 300);
+    _noteText.backgroundColor = _noteColor;
+    _noteText.textColor = [UIColor whiteColor];
+    _noteText.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+    
+    // Note view layout
+    _noteView.frame = CGRectMake(0, CGRectGetMaxY(_locationView.frame), 320, CGRectGetHeight(_noteText.frame)+20);
+    _noteView.backgroundColor = _noteColor;
+    
+    // Note scroll view Layout
+    _noteScrollView.frame = CGRectMake(0, 0, 320, (CGRectGetHeight(_imageView.frame)+CGRectGetHeight(_locationView.frame)+CGRectGetHeight(_noteView.frame)));
+    _noteScrollView.clipsToBounds = NO;
+    _noteScrollView.contentSize=CGSizeMake(320,(CGRectGetHeight(_imageView.frame)+CGRectGetHeight(_locationView.frame)+CGRectGetHeight(_noteView.frame)));
+    
+}
 
 - (void)onAddButtonUp:(id)sender {
     NSLog(@"Button Up");
@@ -77,6 +192,14 @@
     } completion:^(BOOL finished) {
         //
     }];
+}
+
+- (void)onCameraButton:(id)sender {
+    NSLog(@"Take photo!");
+}
+
+- (void)onCameraRollButton:(id)sender {
+    NSLog(@"Choose photo!");
 }
 
 @end
